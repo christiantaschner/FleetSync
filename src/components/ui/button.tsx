5e0ae57be07c6d8529bd431e3a1ed79ac64a30f0
Christian Taschner <christian.taschner.ek@gmail.com>
2025-07-01 14:06:13 +0000

    can you highlight the input fields in white or a lighter grey so its eas

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -14,7 +14,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-input bg-background hover:bg-secondary hover:text-secondary-foreground",
+          "border border-input bg-card hover:bg-secondary hover:text-secondary-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-secondary hover:text-secondary-foreground",
